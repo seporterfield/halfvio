b723bc3a64bcd94a399ac3a7b453841774f884c9
seporterfield <siegfriedporterfield@gmail.com>
2025-11-09 23:14:55 +0100

    move globals into main for easier config

--- perf.py
+++ perf.py
@@ -4,46 +4,52 @@ import csv
 import json
 import uuid
 
-INNER_TASK_REPETITIONS = 10000
-OUTPUT_FILENAME = os.environ["TIMINGS_CSV"]
-HYPERFINE_JSON_FILE = f"/tmp/hyperfine_results_{uuid.uuid4()}.json"
 
-results = []
+def generate_timing_data(commands, output_filename: str, repititions: int):
+    results = []
+    HYPERFINE_JSON_FILE = f"/tmp/hyperfine_results_{uuid.uuid4()}.json"
+    commands_as_shell, command_names = zip(*commands)
 
-COMMANDS = [
-    "nproc --all > /dev/null",
-    "python3 -c 'import os; os.cpu_count()' > /dev/null",
-    "./cnproc > /dev/null"
-]
-COMMAND_NAMES = ["nproc_shell_hyperfine", "python_os_cpu_count_hyperfine", "cnproc"]
+    hyperfine_command = [
+        "hyperfine",
+        "-r",
+        str(repititions),
+        "--export-json",
+        HYPERFINE_JSON_FILE,
+    ]
+    hyperfine_command.extend(commands_as_shell)
 
-hyperfine_command = [
-    "hyperfine",
-    "-r",
-    str(INNER_TASK_REPETITIONS),
-    "--export-json",
-    HYPERFINE_JSON_FILE,
-]
-hyperfine_command.extend(COMMANDS)
+    subprocess.run(
+        hyperfine_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
 
-subprocess.run(
-    hyperfine_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-)
+    with open(HYPERFINE_JSON_FILE, "r") as f:
+        data = json.load(f)
 
-with open(HYPERFINE_JSON_FILE, "r") as f:
-    data = json.load(f)
+    os.remove(HYPERFINE_JSON_FILE)
 
-os.remove(HYPERFINE_JSON_FILE)
+    for i, result in enumerate(data["results"]):
+        method_name = command_names[i]
+        for run_time_s in result["times"]:
+            results.append({"run_time_s": run_time_s, "method": method_name})
 
-for i, result in enumerate(data["results"]):
-    method_name = COMMAND_NAMES[i]
-    for run_time_s in result["times"]:
-        results.append({"run_time_s": run_time_s, "method": method_name})
+    with open(output_filename, mode="w", newline="") as file:
+        fieldnames = ["run_time_s", "method"]
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerows(results)
 
 
-with open(OUTPUT_FILENAME, mode="w", newline="") as file:
-    fieldnames = ["run_time_s", "method"]
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
+def main():
+    output_filename = os.environ["TIMINGS_CSV"]
+    commands = [
+        ("nproc --all > /dev/null", "nproc"),
+        ("python3 -c 'import os; os.cpu_count()' > /dev/null", "python_os_cpu_count"),
+        ("./cnproc > /dev/null", "cnproc"),
+    ]
+    generate_timing_data(commands, output_filename, repititions=1000)
 
-    writer.writeheader()
-    writer.writerows(results)
+
+if __name__ == "__main__":
+    main()
