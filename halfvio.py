e5fbac76a81801970c68661689407519043ab689
seporterfield <siegfriedporterfield@gmail.com>
2025-11-10 00:53:00 +0100

    bubble up testable interface to a small wrapper

--- halfvio.py
+++ halfvio.py
@@ -1,24 +1,29 @@
 import yaml
 import os
 from generate import generate_timing_data
+import sys
 
-def main():
-    with open("timings.yaml", 'r') as f:
+DEFAULT_YAML = "timings.yaml"
+
+
+def halfvio(config_yaml: str = DEFAULT_YAML):
+    with open(config_yaml, "r") as f:
         config = yaml.safe_load(f)
 
     commands_for_timing = [
-        (item['command'], item['name'])
-        for item in config['timing_runs']
+        (item["command"], item["name"]) for item in config["timing_runs"]
     ]
-    
+
     output_file = os.environ["TIMINGS_CSV"]
-    repetitions = config['repetitions']
-    
+    repetitions = config["repetitions"]
+
     generate_timing_data(
-        commands=commands_for_timing, 
-        output_filename=output_file, 
-        repititions=repetitions
+        commands=commands_for_timing,
+        output_filename=output_file,
+        repititions=repetitions,
     )
 
+
 if __name__ == "__main__":
-    main()
+    config_yaml = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_YAML
+    halfvio(config_yaml)
--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -4,6 +4,7 @@ import uuid
 import tempfile
 import pytest
 from typing import Generator
+import yaml
 
 
 @pytest.fixture
@@ -24,11 +25,31 @@ def plot_png() -> Generator[str, None, None]:
         os.unlink(filename)
 
 
-def test_smoke(timings_csv, plot_png) -> None:
+@pytest.fixture
+def config_yaml() -> Generator[str, None, None]:
+    try:
+        filename = os.path.join(tempfile.gettempdir(), f"timing_{uuid.uuid4()}.yaml")
+        yield filename
+    finally:
+        os.unlink(filename)
+
+
+def test_smoke_interface(timings_csv: str, plot_png: str, config_yaml: str) -> None:
+    yaml_config = {
+        "timing_runs": [{"name": "test_ls", "command": "ls"}],
+        "repetitions": 1,
+    }
+
+    with open(config_yaml, "w") as f:
+        yaml.dump(yaml_config, f)
+
     os.environ["TIMINGS_CSV"] = timings_csv
     os.environ["PLOT_PNG"] = plot_png
-    from generate import generate_timing_data
-    
-    generate_timing_data([("ls", "ls")], timings_csv, repititions=1)
+
+    from halfvio import halfvio
+    halfvio(config_yaml)
+
     subprocess.run(["Rscript", "plot.r"], env=os.environ.copy(), check=True)
+
     assert os.path.exists(plot_png)
+    assert os.path.exists(timings_csv)
