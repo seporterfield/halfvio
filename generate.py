93005df3b389579a766d43c46e7ee8c0a45c0c52
seporterfield <siegfriedporterfield@gmail.com>
2025-11-10 02:29:43 +0100

    use ephemeral tempfile instead of saving json

--- generate.py
+++ generate.py
@@ -1,7 +1,5 @@
-import os
 import subprocess
 import json
-import uuid
 import tempfile
 import pandas as pd
 import fastparquet
@@ -11,27 +9,26 @@ def generate_timing_data(
     commands: list[tuple[str, str]], output_filename: str, repititions: int
 ) -> None:
     results = []
-    hyperfine_output = f"{tempfile.gettempdir()}/hyperfine_results_{uuid.uuid4()}.json"
     commands_as_shell, command_names = zip(*commands)
+    with tempfile.NamedTemporaryFile("r+") as hyperfine_output:
+        hyperfine_command = [
+            "hyperfine",
+            "-N",
+            "-r",
+            str(repititions),
+            "--export-json",
+            hyperfine_output.name,
+        ]
+        hyperfine_command.extend(commands_as_shell)
 
-    hyperfine_command = [
-        "hyperfine",
-        "-N",
-        "-r",
-        str(repititions),
-        "--export-json",
-        hyperfine_output,
-    ]
-    hyperfine_command.extend(commands_as_shell)
+        subprocess.run(
+            hyperfine_command,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
 
-    subprocess.run(
-        hyperfine_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-
-    with open(hyperfine_output, "r") as f:
-        data = json.load(f)
-
-    os.unlink(hyperfine_output)
+        data = json.load(hyperfine_output)
 
     for i, result in enumerate(data["results"]):
         method_name = command_names[i]
